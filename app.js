31ca7402d2d5e86b660e0176b614274f07d6eb51
gaurav <gauravsahitya108@gmail.com>
2021-05-08 02:38:59 +0530

    firsh commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express()
+const bodyParser = require('body-parser');
+const port = 3000
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post("/page", (req, res)=>{
+  console.log(req.body.email, req.body.password)
+  if (req.body.email != "" && req.body.password != ""){
+
+    res.sendFile(__dirname + "/page.html")
+  }
+  else{
+    res.redirect("/")
+  }
+})
+
+app.get("/page", (req, res)=>{
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+})
