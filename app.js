914f3538dd1a1b7f1ce3bba6bd57acda688c036b
gaurav <gauravsahitya108@gmail.com>
2021-05-08 17:40:39 +0530

    added layout

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const express = require('express');
 const app = express()
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const clients = JSON.parse(fs.readFileSync("./data/client.json","utf8"))
+
+var index = fs.readFileSync("./index.html","utf8")
+
 const port = 3000
 
+app.use(express.static(__dirname))
 app.use(bodyParser.urlencoded({extended: false}))
 
+var error = false;
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
-
-app.post("/page", (req, res)=>{
-  console.log(req.body.email, req.body.password)
-  if (req.body.email != "" && req.body.password != ""){
-
-    res.sendFile(__dirname + "/page.html")
+  if (!error){
+    res.sendFile(__dirname + '/index.html')
   }
   else{
-    res.redirect("/")
+    res.send(index.replace("DISPLAY",""))
   }
 })
 
-app.get("/page", (req, res)=>{
+
+
+app.post("/page", (req, res)=>{
+  clients.forEach((client, i) => {
+
+    if(client.email == req.body.email && client.password == req.body.password){
+      // console.log(index)
+      res.sendFile(__dirname + "/page.html")
+      return
+    }
+    else{
+      error = true;
+      res.redirect("/")
+    }
+
+  });
 })
 
 
